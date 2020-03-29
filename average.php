e5bf87d491ce5ff0f99ab39ecfb5addf6c4ba9f7
yuusiki54 <yuusiki54@icloud.com>
2020-03-29 11:17:57 +0900

    ；；

--- average.php
+++ average.php
@@ -1,3 +1,14 @@
 <?php
-
+function total($array){
+    $total = 0;
+    for($i=0;$i < count($array);$i++){
+        $total = $total+$array[$i];
+    }
+    return $total;
+}
+$params = array_slice($argv,1);
+$kekka = total($params);
+//$kekka　は合計値が入っている
+$average = $kekka / count([$argv]);
+echo $average;
 ?>
--- hello.php
+++ hello.php
@@ -1,4 +1,8 @@
 <?php
-echo "hello. world";
-echo "ninja";
-echo "hello yuyaaaaaaaaeeeeeeえええええええええええ!";
+$user = [
+    "name" => "yuya", "age" => 26,
+];
+echo $user["age"];
+$user["level"]=18;
+
+?>
